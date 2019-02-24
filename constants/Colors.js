209c072c655790622dbd588a91e0f16ad141ee6f
Cory Hanson <chanson5000@gmail.com>
2019-02-23 23:29:46 -0600

    Initial commit from template

---
+++ constants/Colors.js
@@ -0,0 +1,45 @@
+const tintColor = '#2f95dc';
+const black = '#000000';
+const white = '#ffffff';
+const purpleAccent = '#9c27b0';
+
+export default {
+  defaultTheme: {
+    primary: '#f8bbd0',
+    primaryDark: '#c48b9f',
+    primaryLight: '#ffeeff',
+    accent: purpleAccent,
+    primaryText: black,
+    primaryBackground: '#fce4ec'
+  },
+  redWineTheme: {
+    primary: '#ad1457',
+    primaryDark: '#78002e',
+    accent: purpleAccent,
+    primaryText: white,
+    background: '#fce4ec'
+  },
+  whiteWineTheme: {
+    primary: '#fff8e1',
+    primaryDark: '#ccc5af',
+    primaryLight: white,
+    accent: purpleAccent,
+    primaryTextWhiteTheme: black,
+    backgroundWhiteTheme: '#fce4ec'
+  },
+
+  greyTransparent: '#d6636363',
+  darkGrey: '#757575',
+  errorRed: '#d50000',
+
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+};
