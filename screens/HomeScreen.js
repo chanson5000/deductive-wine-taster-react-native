1020b7cd4b9d8d512c00ee7b2df95207c241edd8
Cory <chanson5000@gmail.com>
2019-02-24 15:41:05 -0600

    Redesigned home screened and removed some template code.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,17 +1,6 @@
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {Image, Platform, StyleSheet, Text, View,} from 'react-native';
 import Colors from '../constants/Colors';
-import { WebBrowser } from 'expo';
-
-import { MonoText } from '../components/StyledText';
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -20,104 +9,37 @@ export default class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <View style={styles.container}>
           <View style={styles.defaultHeaderContainer}>
             <Text style={styles.headerText}>
               Deductive Wine Taster
             </Text>
           </View>
-          <View>
-            <Text style={styles.defaultTitleText}>
-              Start Blind Tasting!
-            </Text>
-          </View>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
-
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+          <View style={styles.mainViewContainer}>
+            <View style={styles.startBlindTastingContainer}>
+              <Text style={styles.defaultTitleText}>
+                Start Blind Tasting!
+              </Text>
             </View>
-
-            <Text style={styles.getStartedText}>
-              Hey look, I changed this text!
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
+            <View style={styles.wineGlassesContainer}>
+              <Image source={require('../assets/images/redwineglass-full-large.png')}/>
+              <Image source={require('../assets/images/whitewineglass-full-large.png')}/>
+            </View>
           </View>
         </View>
-      </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
   defaultHeaderContainer: {
-    backgroundColor: Colors.defaultTheme.primaryBackground,
-    paddingTop: 10,
-    paddingBottom: 10,
+    backgroundColor: Colors.defaultTheme.primaryDark,
+    paddingTop: 30,
+    paddingBottom: 15,
     ...Platform.select({
       ios: {
         shadowColor: 'black',
-        shadowOffset: { height: -3 },
+        shadowOffset: {height: -3},
         shadowOpacity: 0.1,
         shadowRadius: 3,
       },
@@ -130,6 +52,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  mainViewContainer: {
+    flex: 1,
+    backgroundColor: Colors.defaultTheme.primaryBackground,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+  },
   developmentModeText: {
     marginBottom: 20,
     color: 'rgba(0,0,0,0.4)',
@@ -138,7 +66,23 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   contentContainer: {
-    paddingTop: 25,
+    alignItems: 'stretch',
+  },
+  verticallyCenteredContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    backgroundColor: 'yellow',
+  },
+  startBlindTastingContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  },
+  wineGlassesContainer: {
+    justifyContent: 'center',
+    flex: 2,
+    flexDirection: 'row',
   },
   welcomeContainer: {
     alignItems: 'center',
@@ -195,7 +139,7 @@ const styles = StyleSheet.create({
     ...Platform.select({
       ios: {
         shadowColor: 'black',
-        shadowOffset: { height: -3 },
+        shadowOffset: {height: -3},
         shadowOpacity: 0.1,
         shadowRadius: 3,
       },
